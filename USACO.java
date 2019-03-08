a5a7f88422b2fc4ae4d3762043444d8025800498
sbrink00 <sbrink00@stuy.edu>
2019-03-08 09:18:19 -0500

    wrote helper methods to initialize array.

--- USACO.java
+++ USACO.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 public class USACO{
   public static void main(String[]args){
-    try {bronze("test.blah");}
+    try {bronze("lake1.blah");}
     catch (FileNotFoundException e) {e.printStackTrace();}
   }
 
@@ -13,13 +13,25 @@ public class USACO{
     ArrayList<String> lines = new ArrayList<String>();
     initLines(lines, scan);
     int[] info = new int[4];
+    initInfo(info, lines);
+    System.out.println(info[0]);
+    System.out.println(info[1]);
+    System.out.println(info[2]);
+    System.out.println(info[3]);
     //for (String line:lines) System.out.println(line);
     return 1;
   }
 
   private static void initLines(ArrayList<String> ary, Scanner in){while (in.hasNext()) ary.add(in.nextLine());}
-  private static void initInfo(int[] ary){
-    for
+  private static void initInfo(int[] ary, ArrayList<String> s){
+    String info = s.get(0);
+    ary[0] = Integer.parseInt(info.substring(0, info.indexOf(" ")));
+    info = info.substring(info.indexOf(" ") + 1, info.length());
+    ary[1] = Integer.parseInt(info.substring(0, info.indexOf(" ")));
+    info = info.substring(info.indexOf(" ") + 1, info.length());
+    ary[2] = Integer.parseInt(info.substring(0, info.indexOf(" ")));
+    info = info.substring(info.indexOf(" ") + 1, info.length());
+    ary[3] = Integer.parseInt(info);
   }
 
 
